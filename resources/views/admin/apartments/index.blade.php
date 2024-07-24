1662b2aee40b79e974c91c9295d6ca8097531abd
RoccoCerro <rcerro02@gmail.com>
2024-07-24 18:06:37 +0200

    Refactor: style card apartment

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -23,80 +23,76 @@
             <p><a class="btn btn-danger" href="{{ route('admin.apartments.index', ['trash' => 1]) }}">Cestino</a></p>
         @endif
 
-        <div class="row gx-3 gy-3 text-center my-3">
+        <div class="row gy-2 gx-2 flex-wrap row-cols-1 row-cols-md-2 row-cols-lg-3">
             @foreach ($apartments as $apartment)
             @if ($apartment->user_id === Auth::id())
-            <div class="col-12 col-lg-6 position-relative ">
-                <div class="card my-card-apartment h-100 py-3">
-                    <div class="card-body">
-                        <div class="img_banner">
-                            <img src="{{asset('storage/'.$apartment->img_apartment)}}" alt="">
-                            <div class="banner">
-                                <div class="row align-items-center">
-                                    <div class="col">
-                                        <h3 class="my-3">
-                                            {{$apartment->title_apartment}}
-                                        </h3>
-                                    </div>
-                                    @if($apartment->visible == 0)
-                                    <div class="col-auto">
-                                        <i class="fa-solid fa-eye-slash"></i>   
-                                    </div>
-                                    @endif
-                                </div>
+            <div class="col">
+                <div class="card h-100 text-center">
+                    <!-- mostriamo gli appartamenti in evidenza -->
+                
+                    <div class="card-body my_card-body">
 
-                                @if(!$apartment->trashed())
+                        <div class="my-card-title-img">
+
+                            <img src="{{asset('storage/'.$apartment->img_apartment)}}" class="card-img-top img_card" alt="">
+                            <div class="my_card-title">
+                                <div class="my_title">
+                                    <h3 class="my-3">
+                                        {{$apartment->title_apartment}}
+                                    </h3>
+                                </div>
+                                <div class="my-shadow"></div>
+                            </div>
+                        </div>
+
+                        <div class="my-card-text">
+                            @if($apartment->visible == 0)
+                                <div class="p-2">
+                                    <i class="fa-solid fa-eye-slash"></i>   
+                                </div>
+                            @endif
+                            @if(!$apartment->trashed())
                                 <div class="mt-3">
                                     <a class="btn btn-primary" href="{{route('admin.apartments.show',$apartment)}}">Info Appartamento</a>
                                 </div>
-                                @endif
-                                <div>
-                                    {{-- it allows the creation of a button if the apartment is been soft deleted --}}
-                                    @if ($apartment->user_id === Auth::id())
-                                    @if($apartment->trashed())
-                                    {{-- it sent the apartment id to the Apartment Controller through the route --}}
-                                    {{-- <form class="delete-form destroy-form" action="{{ route ('admin.apartments.forceDestroy',$apartment->id) }}"  method="POST">
-                                    
-                                        @csrf
-                                        @method('DELETE')
-                                    
-                                        <button class="btn btn-danger my-3">Elimina definitivamente</button>
-                                    
-                                    </form> --}}
+                            @endif
+                            <div>
+                                {{-- it allows the creation of a button if the apartment is been soft deleted --}}
+                                @if ($apartment->user_id === Auth::id())
+                                @if($apartment->trashed())
 
-                                    <form class="delete-form" action="{{ route ('admin.apartments.restore',$apartment->id) }}"  method="POST">
-                                    
-                                        @csrf
-                                    
-                                        <button class="btn btn-warning my-3">Ripristina</button>
-                                    
-                                    </form>
-
+                                <form class="delete-form" action="{{ route ('admin.apartments.restore',$apartment->id) }}"  method="POST">
+                                
+                                    @csrf
+                                
+                                    <button class="btn btn-warning my-3">Ripristina</button>
+                                
+                                </form>
 
                                 {{-- it creates a button for the soft deleting method --}}
                                 @else
-                                    <form class="delete-form destroy-form" action="{{ route ('admin.apartments.destroy',$apartment) }}"  method="POST">
-                                    
-                                        @csrf
-                                        @method('DELETE')
-                                    
-                                        <button class="btn btn-danger my-3">Elimina</button>
-                                    
-                                    </form>
-                                    <div class="d-none modal-delete position-absolute top-50 start-50 translate-middle rounded p-3 ms_bg-light" >
-                                        <h5>Sei sicuro di voler eliminare?</h5>
-                                        <button class="ms_font-size ms_border ms_hover-si btn-yes btn btn-outline-dark">si</button>
-                                        <button class="ms_font-size ms_border ms_hover-no btn-no btn btn-outline-dark">no</button>
-                                    </div>
-                                    @endif
-                                    @endif
-                                   
+                                <form class="delete-form destroy-form" action="{{ route ('admin.apartments.destroy',$apartment) }}"  method="POST">
+                                
+                                    @csrf
+                                    @method('DELETE')
+                                
+                                    <button class="btn btn-danger my-3">Elimina</button>
+                                
+                                </form>
+                                <div class="d-none modal-delete position-absolute top-50 start-50 translate-middle rounded p-3 ms_bg-light" >
+                                    <h5>Sei sicuro di voler eliminare?</h5>
+                                    <button class="ms_font-size ms_border ms_hover-si btn-yes btn btn-outline-dark">si</button>
+                                    <button class="ms_font-size ms_border ms_hover-no btn-no btn btn-outline-dark">no</button>
                                 </div>
+                                @endif
+                                @endif
+                                
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
             @endif
                 
             @endforeach
